d37733c09acdf1d4b9a0aeee6b6b367c626d4096
wlouthe <wlouthe@gmail.com>
2014-03-09 18:18:14 -0400

    home addclass

--- 490Frontend/home.php
+++ 490Frontend/home.php
@@ -28,6 +28,7 @@
 			$uname = $doc->getElementsByTagName('username')->item(0);
 			$uname = $uname->nodeValue;
             $teacherstudent = $doc->getElementsByTagName('teacherstudent')->item(0)->nodeValue;
+            $id = $doc->getElementsByTagName('id')->item(0)->nodeValue;
 			setcookie('mycode',$mykey,$expire,'/');
 			$cookiechecker=1;
 		}
@@ -35,40 +36,57 @@
 		{
 			echo '<meta http-equiv="refresh" content="1; url=index.php" /></head><body></body>';
 		}
+        if(isset($_POST["iclassname"]))
+        {
+            $url = "http://web.njit.edu/~ss55/490server/addclass.php";
+            $fields = array(
+                'id' => urlencode($id),
+                'classname' => urlencode($_POST["iclassname"])
+            );
+            $coderesult = curlcall($fields,$url);
+        }
 		
 	}
 ?>
 </head>
 <body>
-<div class="mywindow">
+<div class="nav-wrapper">
 <?php
 	if ($cookiechecker==1)
 	{
-		//this should find out whether the user is a teacher or student, their ID and username, and if its a teacher
-		// it should find the teachers classes, the test creation page, and the question creation page.
-		// If its a student, it should find the students classes, and available tests.
-		$url = "http://web.njit.edu/~ss55/490server/gethome.php";
-		$fields = array(
-			'mycode' => urlencode($mykey)
-		);
-		$myresult=curlcall($fields, $url);
-		$doc = new DOMDocument();
-		$doc->loadHTML($myresult);
-		$status = $doc->getElementsByTagName('status')->item(0)->nodeValue;
-		$classes = $doc->getElementsByTagName('classes');
-		echo 
-		"<h3>Welcome ".$uname."!</h3>";
+		echo "Welcome ".$uname."! <a href="logout.php">Logout?</a>";
 	}
 ?>
 	</div>
+
+<div class="main-class">
 <?php
 	if($cookiechecker==1)
 	{
+        //this should find out whether the user is a teacher or student, their ID and username, and if its a teacher
+		// it should find the teachers classes, the test creation page, and the question creation page.
+		// If its a student, it should find the students classes, and available tests.
+		
+        
 		// status==2 is teacher, status==1 is student, and status==0 is unassigned.
-		//if(isset($status) && $status==2)
+		if($teacherstudent==1)
 		{
+            /*
+            $url = "http://web.njit.edu/~ss55/490server/getclasses.php";
+            $fields = array(
+                'txtUsername' => urlencode($uname),
+                'teacherstudent' =>urlencode($teacherstudent)
+            );
+            $myresult=curlcall($fields, $url);
+            $doc = new DOMDocument();
+            $doc->loadHTML($myresult);
+            $status = $doc->getElementsByTagName('status')->item(0)->nodeValue;
+            $classes = $doc->getElementsByTagName('classes');
+            */
+            
 			//show classes. upon clicking classes, displays tests for grading/previously graded,
 			// shows button to create test, shows button to create questions.
+            /*
 			echo "<table><tr><td><div class='mywindow'>";
 			echo "Class:<select name='classes'>";
 				echo "<option value='' selected='selected'></option>";
@@ -77,8 +95,49 @@
 				echo "<option value='".$class->nodeValue."'>".$class->nodeValue."</option>";
 			}
 			echo "</select></div></td><td></td></tr><tr><td></td><td></td></tr></table>";
+            */
+            echo '<table class="main-table">
+<tr>
+<td width=50%>
+<h2 id="currentclasses">Check Classes</h2>
+</td>
+<td width=50%>
+<h2>Add Class</h2>
+<p>
+    <form method ="post" action = "./home.php">
+    Class Name:<input id="iclassname" name="iclassname" type="text">
+    <br>
+    <input type = "submit">
+    <!--<div id="csubmit" class="submit">Submit</div>-->
+    </form>
+</p>
+</td>
+</tr>
+
+<tr>
+<td width=50%>
+<h2>Create Test</h2>
+<p>
+
+</p>
+</td>
+<td  width=50%>
+<h2>Create Questions</h2>
+</td>
+</tr>
+</table>
+';
+            
 		}
+        elseif($teacherstudent == 0)
+        {
+            
+        }
 	}
 ?>
+   
+    </div>
+
+
 </body>
 </html>
