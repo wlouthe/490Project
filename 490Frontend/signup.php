8da775ed86ffb6cbac6c3e6ec47e5d3a8cb0125b
wlouthe <wlouthe@gmail.com>
2014-03-09 15:52:39 -0400

    updated for mycurl and added sending teacherstudent

--- 490Frontend/signup.php
+++ 490Frontend/signup.php
@@ -1,4 +1,5 @@
 <?php
+    require "mycurl.php";
 
 	error_reporting(E_ALL);
 	if(isset($_POST["username"])&&isset($_POST["password"])&&isset($_POST["cpassword"]))
@@ -7,29 +8,11 @@
 		//$url = "http://web.njit.edu/~tjh24/signup.php";
 		$fields = array(
 			'txtEmail' => urlencode($_POST["username"]),
-			'txtPasswd' => urlencode($_POST["password"])
-			
+			'txtPasswd' => urlencode($_POST["password"]),
+            'teacherstudent' => urlencode($_POST["teacherstudent"])
 		);
-		foreach($fields as $key=>$value)
-		{
-			$fields_string .= $key . '=' . $value . '&';
-		}
-		rtrim($fields_string, '&');
-	
-		$ch = curl_init();
-
-		curl_setopt($ch,CURLOPT_URL,$url);
-		curl_setopt($ch,CURLOPT_POST,count($fields));
-		curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
-		curl_setopt($ch,CURLOPT_FOLLOWLOCATION,false);
-		curl_setopt($ch,CURLOPT_MAXREDIRS,0);
-		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-
-		$result = curl_exec($ch);
-
-		curl_close($ch);
-		
-		//echo $result;
+		$result = curlcall($fields,$url);
+		echo $result;
 		
 		// begin dom parsing to check if login was successful
 		$doc = new DOMDocument();
