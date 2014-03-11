7bf38efc4abea87d4432f3488c2b3ff9fe6fde2f
tjhamadeh <tjhamadeh@gmail.com>
2014-03-11 17:35:36 -0400

    add test changes

--- backend/addtest.php
+++ backend/addtest.php
@@ -12,35 +12,30 @@ if (mysqli_connect_errno())
 	echo "MySQL Failed: ".mysqli_connect_error();
 }
 
+
 $TEACHID = $_POST['teachid'];
 $CLASSID = $_POST['classid'];
 $TESTNME = $_POST['testname'];
 
-if (!empty($TEACHID) && !empty($CLASSID))
-{
+if (!empty($TEACHID) && !empty($CLASSID)) {
     $query = mysqli_query($con,"SELECT * FROM login WHERE id = $TEACHID;");
     $row = mysqli_fetch_array($query);
     $status = $row['status'];
-  	if ($status == 1)
-  	{
+  	if ($status == 1) {
         mysqli_query($con,"INSERT INTO test(classId, creatorId, testName, deleteRequest)
             VALUES ($CLASSID, $TEACHID, '$TESTNME', 0);");
-        $getid = mysqli_query($con,"SELECT * FROM test WHERE classId = $CLASSID AND creatorId = $TEACHID AND testName = '$TESTNME';");
-        while ($findMax = mysqli_fetch_array($getid) {
-            $maxId = $findMax['testId'];
-            if ($maxId < $findMax['testId']) {
-                $maxId = $findMax['testId'];
-            }
-        }
+
+        $getid = mysqli_query($con,"SELECT MAX(testId) AS max FROM test WHERE classId = $CLASSID AND creatorId = $TEACHID;");
+        $maxid = mysqli_fetch_array($getid);
+        $TESTID = $maxid['max'];
+        echo "<testid>".$TESTID."</testid>";
     }
-	else
-	{
-		echo "wrong permission";
+	else {
+		echo "Not a teacher.";
 	}
 }
-else
-{
-	echo "how did this happen";
+else {
+	echo "Missing ID or Class ID.";
 }
 
 mysqli_close($con);
